092ce1bdedd943787736e32e7ebdde9481cb4457
Mahmoud Almahroum <mahmoud.al808@gmail.com>
2019-05-26 23:33:44 +0100

    Created String message

--- app/src/main/java/com/example/githubstudy/MainActivity.java
+++ app/src/main/java/com/example/githubstudy/MainActivity.java
@@ -15,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
     public void newFunction(){
 
         Toast.makeText(MainActivity.this, "Branch 01", Toast.LENGTH_SHORT).show();
+        String message = "Hello World";
     }
 }
